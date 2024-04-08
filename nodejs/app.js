c758375ec4c1b54d286c3ac5d33654f2e8e12bc3
kozone <me@kozone>
2024-04-08 23:00:43 +0300

    WIFI Updates

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,36 +1,56 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
-const { spawn } = require('child_process');
+const { spawn, execSync } = require('child_process');
+const cors = require('cors');
+const fs = require('fs');
+const processManager = require('./processManager');
+const wifiManager = require('./wifiManager');
 
 const app = express();
+app.use(cors({
+  origin: function (origin, callback) {
+	if (!origin) return callback(null, true);
+	return callback(null, origin);
+  },
+  credentials: true  // if your frontend includes credentials like cookies
+}));
+
+processManager.init();
+wifiManager.init();
+
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+	origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      return callback(null, origin);
+    },
+	methods: ["GET", "POST"],
+	credentials: true
+  }
+});
+
+
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  let tailProcess;
-
-  socket.on('start-tail', (logFilePath) => {
-    if (tailProcess) tailProcess.kill();
-    tailProcess = spawn('tail', ['-f', logFilePath]);
-
-    tailProcess.stdout.on('data', (data) => {
-      socket.emit('log-update', data.toString());
-    });
+  socket.on('startWifiScan', () => {
+    wifiManager.startWifiScan(io);
   });
 
-  socket.on('stop-tail', () => {
-    if (tailProcess) {
-      tailProcess.kill();
-      tailProcess = null;
-    }
-  });
-
-  socket.on('disconnect', () => {
-    if (tailProcess) tailProcess.kill();
+  socket.on('stopWifiScan', () => {
+    wifiManager.stopWifiScan();
   });
 });
+
+
+function cleanup() {
+	processManager.shutdownServer();
+	wifiManager.cleanup();
+}
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
